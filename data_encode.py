bc35e8f79f068fc91b9909d5eb95424b386631a0
Hannah Leou <hleou@stanford.edu>
2018-06-08 01:49:56 -0700

    beet gone

--- data_encode.py
+++ data_encode.py
@@ -1,18 +1,48 @@
-import 
+import numpy as np
+import mido
+from sys import argv
 
-def to_one_hot(midi_msg) {
-	
-}
-def extractMelody(midi_file, new_file_name):
-	mid = mido.MidiFile();
-	track = mido.MidiTrack();
-	mid.tracks.append(track);
-	for msg in mido.MidiFile(midi_file).tracks[0]: 
-		if msg.type == 'note_on' and msg.channel == 0:
-			track.append(msg)
-		elif msg.type == 'note_off' and msg.channel == 0: # Transform note_off message to a note_on message by changing the velocity to 0 (turns msg.note off)
-			track.append(mido.Message('note_on', note = msg.note, velocity = 0, time = msg.time))
-		else:
-			track.append(msg)
-	mid.save(new_file_name)
-	print("Done stripping " + midi_file + " ... saved as " + new_file_name)
+
+def extractEncoding(midi_file, encoding_file):
+	array = np.zeros((1,128))
+	vector = array[0]
+	for msg in mido.MidiFile(midi_file).tracks[0]:
+		if (msg.type == 'note_on' and msg.velocity != 0):
+			array = np.vstack([array, vector])
+			vector[msg.note] = 1
+		elif (msg.type == 'note_off' or (msg.type == 'note_on' and msg.velocity == 0)):
+			array = np.vstack([array, vector])
+			vector[msg.note] = 0
+   	np.save(encoding_file + '.npy', array)
+   	print 'Done with encoding for ' + midi_file
+
+def extractAllEncodings(path_name, num_encodings):
+	for i in range (1, num_encodings + 1):
+		midi_file = path_name + '/' + str(i) + '_out.mid'
+		encoding_file = path_name + '/' + str(i) + '_encoding'
+		extractEncoding(midi_file, encoding_file)
+
+def smashAllEncodings(path_name, num_encodings, cat_encoding_file):
+	smashedEncodings = np.load(path_name + '/' + str(1) + '_encoding.npy')
+	for i in range (2, num_encodings + 1):
+		encoding = np.load(path_name + '/' + str(i) + '_encoding.npy')
+		smashedEncodings = np.vstack([smashedEncodings, encoding])
+	np.save(cat_encoding_file + '.npy', smashedEncodings)
+
+if __name__ == '__main__':
+	mode = argv[1]
+	if (mode == '--single'):
+		midi_file = argv[2]
+		encoding_file = argv[3]
+		extractEncoding(midi_file, encoding_file)
+	elif (mode == '--all'):
+		path_name = argv[2]
+		num_encodings = argv[3]
+		extractAllEncodings(path_name, int(num_encodings))
+	elif (mode == '--smash'):
+		path_name = argv[2]
+		num_encodings = argv[3]
+		cat_encoding_file = argv[4]
+		smashAllEncodings(path_name, int(num_encodings), cat_encoding_file)
+	else :
+		print 'Wrong Usage'
